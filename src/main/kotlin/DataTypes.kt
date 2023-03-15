49a1d75c0ad991c62430469864311c37803a2c8d
clencyo <audiajepkirui123@gmail.com>
2023-03-15 08:53:38 +0300

    data type in kotlin

---
+++ src/main/kotlin/DataTypes.kt
@@ -0,0 +1,23 @@
+fun main(args: Array<String>) {
+
+    //Int
+    var age = 20
+    var age: Int = 20
+
+    //Strings
+    var name = "dj Joe"
+    var name: String = "dj JOe"
+
+    //Chars
+    var city = 'K'
+
+    //Booleans
+    var status = true
+    var ststus = false
+
+    //Double
+    var temp = 36.57
+
+    //Float
+    var lat:Float = 36.75
+}
