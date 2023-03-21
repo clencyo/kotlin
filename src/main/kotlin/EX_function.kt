adbb7f9165109d9d48af1e7179e19fe755577f94
clencyo <audiajepkirui123@gmail.com>
2023-03-21 10:02:28 +0300

    my second commit

---
+++ src/main/kotlin/EX_function.kt
@@ -0,0 +1,27 @@
+fun main(args: Array<String>) {
+    //create a function that add two no
+    //sum 200
+    //create another function to mult two no result be 2000
+    //divide and result be 20
+    main(num1:400, num2:400)
+    two()
+    three()
+}
+
+fun main(num1:int, num2:int) {
+    var result = num1 + num2
+    println(result)
+
+}
+fun two() {
+    val val1 = 100
+    val val2 = 20
+    val mult = val1 * val2
+    println(mult)
+}
+fun three() {
+    val val1 = 2000
+    val val2 = 100
+    val div = val1 / val2
+    println(div)
+}
---
+++ src/main/kotlin/functions.kt
@@ -0,0 +1,14 @@
+fun main(args: Array<String>) {
+    sayhello()
+    today()
+
+}
+
+
+fun sayhello() {
+    println("Good mornig people")
+}
+
+fun today(){
+    println("Lets go out")
+}
